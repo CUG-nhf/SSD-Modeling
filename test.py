0f76a1e757dc1c8fe35f4e44cb401fbb29a6bfd2
mac <nhf17634401346@163.com>
2023-11-02 14:41:19 +0800

    modified:   test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ PE = [1000, 5000, 7000, 10000]
 
 t = ssdModel(block_error_rate, cnt, PE)
 print('ssd\t\t  ',t.get_SSD_error_rate())
-print('channel\t' ,t.channel_error_rate)
+print('channel\t  ' ,t.channel_error_rate)
 print('chip\t  ', t.chip_error_rate)
 print('die\t\t  ', t.die_error_rate)
 print('block\t  ', t.block_error_rate)
