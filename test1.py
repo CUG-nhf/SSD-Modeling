1c5f840e941b24c652d6a9ae2bef72369f2e2809
CUG-nhf <nhf17634401346@163.com>
2023-11-12 22:25:55 +0800

    git commit

--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def fig1():
 	#ALCod: Adaptive LDPC Coding for 3D NAND Flash Memory Using Inter-Layer RBER Variation
@@ -14,7 +15,45 @@ def fig1():
 	plt.xlabel("RBER($10^{-2}$)")
 	plt.ylabel("UBER")
 	plt.legend(loc ='best')
+	plt.grid('on',which='both')
 	# plt.savefig('picture/{}b-{}b-binom-'.format(E, N) + formatted_datetime)
 	plt.show()
 
-fig1()
+def fig2():
+	#A 38.64-Gb/s Large-CPM 2-KB LDPC Decoder Implementation for nand Flash Memories
+	
+	rber = [1.26,1.34,1.39,1.44,1.51,1.61,1.72,1.82]
+	uber = [3e-10,4e-8,5e-7,5e-6,6e-5,6e-4,3e-3,8e-3]
+	plt.plot(rber, uber, '-v', label='Proposed')
+
+	rber = [1,1.12,1.4,1.51,1.59,1.68,1.82]
+	uber = [1e-5,2e-5,1.5e-4,4e-4,2e-3,4e-3,9e-3]
+	plt.plot(rber, uber, '-*', label='ISCAS\'21')
+
+	rber = [0.76, 0.9, 1.0, 1.1, 1.2]
+	uber = [1e-11,3e-7,5e-5,2e-3,9e-3]
+	plt.plot(rber, uber, '-x', label='TCAS-II\'18')
+
+	rber = [1.28,1.36,1.45,1.55,1.65]
+	uber = [6e-6,1.5e-4,1.5e-3,6e-3,9e-3]
+	plt.plot(rber, uber, '-^', label='TCAS-I\'17')
+
+	rber = [1.05,1.13,1.21,1.3,1.38,1.47,1.67]
+	uber = [3e-10,1e-7,7e-6,2e-4,2e-3,6e-3,8e-3]
+	plt.plot(rber, uber, '-o', label='TVLSI\'16')
+
+	plt.xlim(0.7, 1.9)
+	plt.ylim(5e-12, 1e-1)
+	plt.yscale('log')
+	plt.xlabel("RBER(%)")
+	plt.ylabel("UBER(log10)")
+	plt.legend(loc ='best')
+	plt.grid('on',which='both')
+	plt.title('2KB LDPC')
+	# plt.savefig('picture/{}b-{}b-binom-'.format(E, N) + formatted_datetime)
+	plt.show()
+
+
+fig2()
+
+
