fe860c184f1ac662a736f67355c0595f69a56f24
mac <nhf17634401346@163.com>
2023-12-12 13:11:33 +0800

    git commit

--- fit_power_function.py
+++ fit_power_function.py
@@ -32,9 +32,12 @@ def fit(rber, uber, needLog=True):
 	# Plot raw data and fitted curve
 	plt.scatter(rber, uber, label='Raw Data')
 	
-	
-	plt.xlabel("$log_{10}(RBER)$")
-	plt.ylabel("$log_{10}(UBER)$")
+	if needLog:
+		plt.xlabel("$log_{10}(RBER)$")
+		plt.ylabel("$log_{10}(UBER)$")
+	else:
+		plt.xlabel("RBER")
+		plt.ylabel("UBER")
 	plt.legend(loc ='best')
 	plt.grid('on',which='both')
 	plt.show()
@@ -76,37 +79,9 @@ def fits(rbers, ubers, labels, colors, needLog=True):
 
 
 if	__name__ == '__main__':
-	rbers, ubers= [], []
 
-# '-v', label='Proposed'
-	rber = [1.26e-2,1.34e-2,1.39e-2,1.44e-2,1.51e-2,1.61e-2,1.72e-2,1.82e-2]
-	uber = [3e-10,4e-8,5e-7,5e-6,6e-5,6e-4,3e-3,8e-3]
-	rbers.append(rber)
-	ubers.append(uber)
-# '-*', label='ISCAS\'21'
-	rber = [1e-2,1.12e-2,1.4e-2,1.51e-2,1.59e-2,1.68e-2,1.82e-2]
-	uber = [1e-5,2e-5,1.5e-4,4e-4,2e-3,4e-3,9e-3]
-	rbers.append(rber)
-	ubers.append(uber)
-# '-x', label='TCAS-II\'18'
-	rber = [0.76e-2, 0.9e-2, 1.0e-2, 1.1e-2, 1.2e-2]
-	uber = [1e-11,3e-7,5e-5,2e-3,9e-3]
-	rbers.append(rber)
-	ubers.append(uber)
-# '-^', label='TCAS-I\'17
-	rber = [1.28e-2,1.36e-2,1.45e-2,1.55e-2,1.65e-2]
-	uber = [6e-6,1.5e-4,1.5e-3,6e-3,9e-3]
-	rbers.append(rber)
-	ubers.append(uber)
-# '-o', label='TVLSI\'16'
-	rber = [1.05e-2,1.13e-2,1.21e-2,1.3e-2,1.38e-2,1.47e-2,1.67e-2]
-	uber = [3e-10,1e-7,7e-6,2e-4,2e-3,6e-3,8e-3]
-	rbers.append(rber)
-	ubers.append(uber)
-	labels = ['Proposed', 'ISCAS\'21', 'TCAS-II\'18', 'TCAS-I\'17', 'TVLSI\'16']
-	colors = ['red', 'brown', 'green', 'black', 'blue']
-	# labels = ['LDPC(2KB,0.90-rate)', 'LDPC(8KB,0.96-rate)', 'LDPC(4KB,0.90-rate)']
-	# colors = ['green', 'purple', 'blue']
-	fits(rbers, ubers, labels, colors)
-	
+	rber = [0.75e-2, 0.83e-2, 0.89e-2, 1.0e-2, 1.18e-2, 1.342e-2, 1.5e-2, 1.75e-2, 1.94e-2]
+	uber = [6.579e-7, 5.921e-6, 1.711e-5, 5.789e-5, 7.763e-5,7.752e-5, 7.742e-5, 7.732e-5, 7.732e-5]
+
+	fit(np.array(rber), np.array(uber))
 
